17714f50208264f58f7668f4544c5bb3c06bcb69
abbas <abbas.devcode@gmail.com>
2023-05-03 09:11:35 +0530

    added community page routing

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,49 @@
+import { Community } from "@/src/atoms/communitiesAtom";
+import CommunityNotFound from "@/src/components/Community/CommunityNotFound";
+import { firestore } from "@/src/firebase/clientApp";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import React from "react";
+import safeJsonStringify from "safe-json-stringify";
+
+type CommunityPageProps = {
+  communityData: Community;
+};
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  if (!communityData) {
+    return <CommunityNotFound />;
+  }
+
+  return <div>Welcome to {communityData.id}</div>;
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  /// get community data and pass to client component
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+
+    return {
+      props: {
+        communityData: !communityDoc.exists()
+          ? ""
+          : JSON.parse(
+              safeJsonStringify({
+                id: communityDoc.id,
+                ...communityDoc.data(),
+              })
+            ),
+      },
+    };
+  } catch (error) {
+    /// can add an error page here
+    console.log("getServerSideProps error: ", error);
+  }
+}
+
+export default CommunityPage;
